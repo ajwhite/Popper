cc067a3b44b8f02703de08654137d50f1f3af173
Atticus White <contact@atticuswhite.com>
2015-09-11 02:15:23 -0400

    base implementation

--- src/popper.js
+++ src/popper.js
@@ -6,19 +6,55 @@
     this.poppers = this.element.find(':not([data-primary])');
 
     this.primary.on('click', this.primaryClicked.bind(this));
+    this.element.addClass('popper-container');
+    this.poppers.addClass('secondary');
+    this.primary.addClass('primary');
     this.hidePoppers();
   }
 
+  Popper.prototype.getPoppersStartingPosition = function () {
+    return {
+      left: Math.abs(this.primary.position().left + (this.primary.width()/2)),
+      top: Math.abs(this.primary.position().top + (this.primary.height()/2))
+    };
+  };
+
   Popper.prototype.hidePoppers = function () {
-    this.poppers.hide();
+    var position = this.getPoppersStartingPosition();
+    this.poppers.each(function () {
+      $(this).css({
+        left: position.left - ($(this).width() / 2),
+        top: position.top - ($(this).width()/2)
+      });
+    });
+    this.element.removeClass('popped');
   };
 
   Popper.prototype.showPoppers = function () {
+    this.element.addClass('popped');
     this.poppers.show();
+    this.positionPoppers();
+  };
+
+  Popper.prototype.positionPoppers = function () {
+    var radius = 100,
+        angle = 0,
+        step = (2 * Math.PI) / this.poppers.length;
+
+    this.poppers.each(function (item, index) {
+      var x = Math.round(32 + radius * Math.cos(angle) - ($(this).width()/2)),
+          y = Math.round(32 + radius * Math.sin(angle) - ($(this).height()/2));
+      $(this).css({left: x + 'px', top: y + 'px'});
+      angle += step;
+    });
   };
 
   Popper.prototype.primaryClicked = function () {
-    this.showPoppers();
+    if (this.element.hasClass('popped')) {
+      this.hidePoppers();
+    } else {
+      this.showPoppers();
+    }
   };
 
 
