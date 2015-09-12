f857f96afaa4ad089fb0791e4cf7202417bf94d6
Atticus White <contact@atticuswhite.com>
2015-09-12 02:05:04 -0400

    minify build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,44 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    rename = require('gulp-rename');
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    minifyCss = require('gulp-minify-css'),
+    paths = {
+      js: 'src/**/*.js',
+      less: 'src/**/*.less'
+    };
 
 gulp.task('js', function () {
-  gulp.src('src/**/*.js')
+  gulp.src(paths.js)
   .pipe(rename('popper.js'))
   .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('less', function () {
-  gulp.src('src/**/*.less')
-  .pipe(less())
+gulp.task('js:min', function () {
+  gulp.src(paths.js)
+  .pipe(uglify())
+  .pipe(rename('popper.min.js'))
   .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', ['js', 'less']);
+gulp.task('less', function () {
+  gulp.src(paths.less)
+  .pipe(less())
+  .pipe(rename('popper.css'))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('less:min', function () {
+  gulp.src(paths.less)
+  .pipe(less())
+  .pipe(minifyCss())
+  .pipe(rename('popper.min.css'))
+  .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build', ['js', 'js:min', 'less', 'less:min']);
 gulp.task('default', ['build']);
 gulp.task('watch', ['build'], function () {
-  gulp.watch('src/**/*.js', ['js']);
-  gulp.watch('src/**/*.less', ['less']);
+  gulp.watch('src/**/*.js', ['js', 'js:min']);
+  gulp.watch('src/**/*.less', ['less', 'less:min']);
 });
